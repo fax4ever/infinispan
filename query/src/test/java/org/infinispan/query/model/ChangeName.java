01bcb1046982ded0fb5cc979b6b0bc5a20660d37
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-10-16 10:23:12 +0200

    ISPN-13523 Test changing index property name

--- query/src/test/java/org/infinispan/query/model/ChangeName.java
+++ query/src/test/java/org/infinispan/query/model/ChangeName.java
@@ -3,28 +3,15 @@ package org.infinispan.query.model;
 import org.infinispan.api.annotations.indexing.Basic;
 import org.infinispan.api.annotations.indexing.Indexed;
 import org.infinispan.protostream.GeneratedSchema;
-import org.infinispan.protostream.annotations.ProtoFactory;
-import org.infinispan.protostream.annotations.ProtoField;
+import org.infinispan.protostream.annotations.Proto;
 import org.infinispan.protostream.annotations.ProtoSchema;
 
+@Proto
 @Indexed
-public class ChangeName {
+public record ChangeName(@Basic(name = INDEX_FIELD_NAME) String dataProperty) {
 
    public static final String DATA_FIELD_NAME = "dataProperty";
    public static final String INDEX_FIELD_NAME = "indexProperty";
-
-   private String dataProperty;
-
-   @ProtoFactory
-   public ChangeName(String dataProperty) {
-      this.dataProperty = dataProperty;
-   }
-
-   @ProtoField(number = 1)
-   @Basic(name = INDEX_FIELD_NAME)
-   public String getDataProperty() {
-      return dataProperty;
-   }
 
    @ProtoSchema(
          includeClasses = ChangeName.class,
